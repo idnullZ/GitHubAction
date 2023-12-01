b49d73377c093dbb70ee39332899a94b2a9f508a
Нурахмет Зунун <nzunun@n1broker.kz>
2023-12-01 20:30:59 +0600

    Initial commit

---
+++ app/src/main/java/kz/zunun/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package kz.zunun.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var counter by mutableStateOf(0)
+
+        setContent {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Button(
+                    modifier = Modifier.align(Alignment.Center),
+                    onClick = { counter++ },
+                    content = {
+                        Text(text = "Count $counter")
+                    }
+                )
+            }
+        }
+    }
+}
