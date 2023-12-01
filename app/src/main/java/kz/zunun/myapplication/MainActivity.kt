47775819179a4b463ce31b33ce74e7875637a048
Нурахмет Зунун <nzunun@n1broker.kz>
2023-12-01 20:40:41 +0600

    second commit

--- app/src/main/java/kz/zunun/myapplication/MainActivity.kt
+++ app/src/main/java/kz/zunun/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -26,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.align(Alignment.Center),
                     onClick = { counter++ },
                     content = {
-                        Text(text = "Count $counter")
+                        Text(text = "Counter $counter")
                     }
                 )
             }
